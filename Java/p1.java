f960c80700d0efde8fadffc2816adc823bc2c6f6
DebugDynamo01 <bhardwajhardik46@gmail.com>
2026-02-03 20:10:56 +0530

    Added java folder

---
+++ Java/p1.java
@@ -0,0 +1,10 @@
+class p1 {
+    public static void main(String ar[]){
+        int x1=100;//100
+		int x2=0100;//64
+		int x3=0x100;//256
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+    }
+}
---
+++ Java/p2.java
@@ -0,0 +1,12 @@
+class p2 {
+    public static void main(){
+        byte b1=100;
+		byte b2=100;
+		byte b3;
+		b3=(byte)(b1+b2);
+		int x=260;
+		byte b4=(byte)x;
+		System.out.println(b3);
+		System.out.println(b4);
+    }
+}
